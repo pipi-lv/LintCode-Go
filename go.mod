2213e5d9a618da181d5a671bb7348d9d2b616880
pipi-lv <1447191636@qq.com>
2021-12-06 22:56:20 +0800

    第一天打卡

---
+++ go.mod
@@ -0,0 +1,3 @@
+module LintCode-Go
+
+go 1.17
---
+++ src/aplusb.go
@@ -0,0 +1,11 @@
+package main
+
+/**
+ * @param a: An integer
+ * @param b: An integer
+ * @return: The sum of a and b
+ */
+func aplusb(a int, b int) int {
+	// write your code here
+	return a + b
+}
