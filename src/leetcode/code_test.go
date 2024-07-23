c3cca5969728a8cc413889aefa585277ca2a1324
pipi-lv <1447191636@qq.com>
2024-07-23 23:08:38 +0800

    feat: add day exercise

--- src/leetcode/code_test.go
+++ src/leetcode/code_test.go
@@ -9,3 +9,7 @@ func TestAccountsMerge(t *testing.T) {
 func TestMinimumTime(t *testing.T) {
 	t.Log(minimumTime(6, [][]int{{0, 4, 7}, {3, 3, 10}, {4, 4, 10}, {1, 0, 1}, {5, 5, 1}, {1, 4, 6}, {3, 5, 9}, {4, 1, 6}, {2, 2, 7}, {3, 4, 2}}, []int{20, 10, 5, 7, 16, 20}))
 }
+
+func TestMaximumSum(t *testing.T) {
+	t.Log(maximumSum([]int{100, 30, 1, 987, 400, 200, 9}))
+}
